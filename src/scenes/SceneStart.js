03cc3921d981a97cd89e874c97a5444624c8c908
ZackyDacky <robloxzacfiel@gmail.com>
2021-03-14 13:44:15 +0800

    Bug fixes 2

--- src/scenes/SceneStart.js
+++ src/scenes/SceneStart.js
@@ -41,11 +41,11 @@ export default class SceneStart extends Phaser.Scene {
         }, this )
         this.buttonSquare.setOrigin(0.5, 0.5)
         
-        this.mainBG = this.add.image(0, 0, 'mainBG')
-        this.mainBG.scaleX = 0.11375
-        this.mainBG.scaleY = 0.11375
-        this.mainBG.setOrigin(0, 0)
-        console.log("Ready!!!!!");
+        // this.mainBG = this.add.image(0, 0, 'mainBG')
+        // this.mainBG.scaleX = 0.11375
+        // this.mainBG.scaleY = 0.11375
+        // this.mainBG.setOrigin(0, 0)
+        // console.log("Ready!!!!!");
         // this.playButtonOP = this.add.image(198, 550, 'PlayButtonOP')
         // this.playButtonOP.scaleX = 0.15
         // this.playButtonOP.scaleY = 0.15
