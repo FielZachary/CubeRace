635ec8e1bb6c8dbc14e46252f34ac27267d10aac
ZackyDacky <robloxzacfiel@gmail.com>
2021-03-14 16:44:03 +0800

    More fixes to mobile scaling issue

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ if (isMobile == -1) {
     var config = {
         type: Phaser.AUTO,
         scale: {
-            mode: Phaser.Scale.FIT,
+            mode: Phaser.Scale.ENVELOP,
             _parent: 'phaser-example',
             width: 506,
             height: 800,
